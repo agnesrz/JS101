2f5797899d0fd150d256b219b2775b6ce44f8953
Agnes Rzepecki <aga.r909@gmail.com>
2020-07-06 20:15:24 +0000

    adding completed exercise

--- easy_3/ex_02.js
+++ easy_3/ex_02.js
@@ -61,48 +61,24 @@ logInBox('');
 
 // Further Exploration, Part 2
 
-function getBoxText(string, maxCharLimit) {
+function getBoxText(string, maxChar) {
+
   let resultsArray = [];
   let startValue = 0;
-  let endValue = maxCharLimit - 1;
+  let endValue = maxChar - 1;
   let again = true;
 
   while (again) {
     let tempArray = ['| '];
 
     for (let i = startValue; i < endValue; i += 1) {
-      if (string[i] === undefined) {
-        tempArray.push(' ');
-      } else {
-        tempArray.push(string[i]);
-      }
+      tempArray.push(string[i] === undefined ? ' ' : string[i]);
     }
 
-    if (string[endValue] === undefined) { // if the entire string has ended
-      tempArray.push('  |');
-      again = false;
-      console.log(1);
-    } else if ((string[endValue - 1] === ' ') && (string[endValue + 1] !==  ' ')) { // if 2nd-to-last char is ' ' and nxt word has more than 1 char
-      tempArray.push('  |');
-      startValue += maxCharLimit - 1;
-      endValue += maxCharLimit - 1;
-      console.log(2);
-    } else if (string[endValue] === ' ') { // if last character is a space
-      tempArray.push('  |');
-      startValue += maxCharLimit;
-      endValue += maxCharLimit;
-      console.log(3);
-    } else if (string[endValue + 1] === ' ') { // if first character in next iteration is a space
-      tempArray.push(string[endValue] + ' |');
-      startValue += maxCharLimit + 1;
-      endValue += maxCharLimit + 1;
-      console.log(4);
-    } else {                  // if line break happens in middle of a word
-      tempArray.push('- |');
-      startValue += maxCharLimit - 1;
-      endValue += maxCharLimit - 1;
-      console.log(5);
-    }
+    tempArray.push(valueToPush(string[endValue - 1], string[endValue], string[endValue + 1]));
+    again = string[endValue] !== undefined;
+    startValue += updateStartAndEndValues(string[endValue], string[endValue + 1], maxChar);
+    endValue += updateStartAndEndValues(string[endValue], string[endValue + 1], maxChar);
 
     resultsArray.push(tempArray.join(''));
   }
@@ -110,18 +86,41 @@ function getBoxText(string, maxCharLimit) {
 }
 
 
+function valueToPush(value1, value2, value3) {
+
+  if ((value2 === undefined) || (value2 === ' ')) {
+    return '  |';
+  } else if ((value1 === ' ') && (value3 !== ' ')) {
+    return '  |';
+  } else if (value3 === ' ') {
+    return value2 + ' |';
+  } else {
+    return '- |';
+  }
+}
+
+
+function updateStartAndEndValues(value2, value3, maxChar) {
+
+  if (value2 === ' ') {
+    return maxChar;
+  } else if (value3 === ' ') {
+    return maxChar + 1;
+  } else {
+    return maxChar - 1;
+  }
+}
+
 function logInBox(string, width) { // function assumes width is >= 6
 
   let boxLinesTopAndBottom;
   let boxLinesAboveAndBelowText;
   let boxLinesText;
 
-  let maxCharLimit = width - 4;
-
-  if (string.length > maxCharLimit) {
-    boxLinesTopAndBottom = '+-' + '-'.repeat(maxCharLimit) + '-+';
-    boxLinesAboveAndBelowText = '| ' + ' '.repeat(maxCharLimit) + ' |';
-    boxLinesText = getBoxText(string, maxCharLimit);
+  if (string.length > width - 4) {
+    boxLinesTopAndBottom = '+-' + '-'.repeat(width - 4) + '-+';
+    boxLinesAboveAndBelowText = '| ' + ' '.repeat(width - 4) + ' |';
+    boxLinesText = getBoxText(string, width - 4);
 
   } else {
     boxLinesTopAndBottom = '+-' + '-'.repeat(string.length) + '-+';
@@ -136,5 +135,7 @@ function logInBox(string, width) { // function assumes width is >= 6
   console.log(boxLinesTopAndBottom);
 }
 
-logInBox('To boldly go where no one has gone before.', 10);
+logInBox('To boldly go where no one has gone before.', 15);
+logInBox('To boldly go where no one has gone before.', 30);
+logInBox('To boldly go where no one has gone before.', 6);
 logInBox('');
